4ed3ad3c4908c1e26a0e3821de1cba917cb37cda
as6620 <as6620@bs.amalnet.k12.il>
2023-12-21 13:10:42 +0200

    second

--- app/src/main/java/com/example/task52/MainActivity.java
+++ app/src/main/java/com/example/task52/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.task52;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
