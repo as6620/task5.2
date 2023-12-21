e00b79df114baf5949782f3d0fbfdf5bfc912614
as6620 <as6620@bs.amalnet.k12.il>
2023-12-21 13:22:51 +0200

    final

--- app/src/main/java/com/example/task52/MainActivity.java
+++ app/src/main/java/com/example/task52/MainActivity.java
@@ -2,17 +2,28 @@ package com.example.task52;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
-    TextView textView;
+    TextView tv;
+    int counterClick = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        tv = findViewById(R.id.tv);
+    }
+
+    public void clicked(View view) {
+        tv.setTextColor(Color.RED);
+        counterClick++;
+        tv.setText("This is a click number: " + counterClick);
 
     }
 }
